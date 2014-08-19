cb70572a914de0a8d50eb7825169b55fefcf18bf
EIA-MO <angelliya00@gmail.com>
2014-08-19 10:55:20 +0800

    descript update

--- TweenTag.js
+++ TweenTag.js
@@ -1,7 +1,7 @@
  /*
  * @author Eia (http://github.com/EIA http://about.me/eia)
  *
- * for feature requests or bugs, please visit https://github.com/EIA/TweenTag.js/blob/master/TweenTag.js
+ * for feature requests or bugs, please visit https://github.com/EIA/TweenTag.js
  *
  * licensed under the MIT license 
  */
